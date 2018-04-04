c1bca4c190a6fcaaeeb771356dd432425f93d42a
Jerome <jpdeleon.bsap@gmail.com>
2018-04-04 12:42:32 +0900

    fixed issue with lxml adding in dependency

--- setup.py
+++ setup.py
@@ -51,5 +51,5 @@ setup(
         'Topic :: Scientific/Engineering',
         'Programming Language :: Python'
         ],
-    install_requires = ['BeautifulSoup4','urllib3'],
+    install_requires = ['BeautifulSoup4','urllib3','lxml'],
 )
